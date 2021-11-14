cba37b2a169fff7799958ea479df8bd51b0fff45
Arina <rina.runet@gmail.com>
2021-11-14 20:59:18 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import './App.css';
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
-// import { Form } from "./components/Form";
-// import { MessageList } from "./components/MessagesList";
 import { AUTHORS } from "./utils/constants";
 
 import Chats from "./components/Chats";
@@ -15,8 +13,6 @@ import ChatList from "./components/ChatsList";
 
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-// import ListItemText from '@mui/material/ListItemText';
-// import ListItemButton from '@mui/material/ListItemButton';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { store } from "./store";
@@ -61,70 +57,8 @@ function App() {
     },
   });
 
-  // const messagesList = [];
-
-  // const [messages, setMessages] = useState(messagesList);
-  // const parentRef = useRef();
-  // const [count, setCount] = useState(0);
-
-  // const handleSendMessage = useCallback((newMessage) => {
-  //   setMessages((prevMessages) => [...prevMessages, newMessage]);
-  // }, []);
-
   const [chatList, setChatList] = useState(initialChatList);
   const [messages, setMessages] = useState(initialMessages);
-
-  // useEffect(() => {
-  //   if (messages.length && messages[messages.length - 1].author !== 'Bot') {
-  //     const timeout = setTimeout(
-  //       () =>
-  //         handleSendMessage({
-  //           author: AUTHORS.bot,
-  //           text: "Hello, how can I help you?",
-  //           id: `bot-message-${count}`,
-  //         }),
-  //       1000
-  //     );
-  //     setCount(count + 1);
-
-  //     return () => clearTimeout(timeout);
-  //   }
-  // }, [messages]);
-
-  // return (
-  //   <ThemeProvider theme={customTheme}>
-  //     <div className="App" ref={parentRef}>
-  //       <div className="wrapper">
-  //         <div className="container">
-  //           <div className="chatList">
-  //             <div className="chatListHeader">Chats list</div>
-              // <List disablePadding>
-              //   <ListItem disablePadding>
-              //     <ListItemButton>
-              //       <ListItemText primary="One" />
-              //     </ListItemButton>
-              //   </ListItem>
-              //   <ListItem disablePadding>
-              //     <ListItemButton>
-              //       <ListItemText primary="Two" />
-              //     </ListItemButton>
-              //   </ListItem>
-              //   <ListItem disablePadding>
-              //     <ListItemButton>
-              //       <ListItemText primary="Three" />
-              //     </ListItemButton>
-              //   </ListItem>
-              // </List>
-  //           </div>
-            // <div className="chat">
-            //   <MessageList messages={messages} />
-            //   <Form onSendMessage={handleSendMessage} />
-            // </div>
-      //     </div>
-      //   </div>
-      // </div>
-  //   </ThemeProvider>
-  // );
 
   return (
     <ThemeProvider theme={customTheme}>
